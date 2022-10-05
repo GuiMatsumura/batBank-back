ded4e39d461121b64b0bd716804d9c34836d58c8
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2022-10-05 01:42:32 -0300

    :sparkles: feat: use error handler middleware on app

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import cors from 'cors';
 import express, { json } from 'express';
 import 'express-async-errors';
 import router from './routes/index';
+import errorHandlerMiddleware from './middlewares/errorHandlerMiddleware';
 
 const app = express();
 
 app.use(cors());
 app.use(json());
 app.use(router);
+app.use(errorHandlerMiddleware);
 
 export default app;
