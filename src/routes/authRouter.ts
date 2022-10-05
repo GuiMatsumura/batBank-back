b64c28f93a5788ef381f45cbf15fd025f704b716
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2022-10-05 01:43:46 -0300

    :sparkles: feat: user validate schema middleware on signup

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { signUp } from '../controllers/authController';
+import { validateSchemaMiddleware } from '../middlewares/validateSchemaMiddleware';
+import { signUpSchema } from '../schemas/authSchema';
 
 const authRouter = Router();
 
-authRouter.post('/signup', signUp);
+authRouter.post('/signup', validateSchemaMiddleware(signUpSchema), signUp);
 
 export default authRouter;
