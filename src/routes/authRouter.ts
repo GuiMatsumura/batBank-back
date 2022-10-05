f056e2b1a051d4667c7f542b7ef40f180579b6f0
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2022-10-05 02:36:04 -0300

    :sparkles: feat: add user signin router

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { signUp } from '../controllers/authController';
+import { signUp, signIn } from '../controllers/authController';
 import { validateSchemaMiddleware } from '../middlewares/validateSchemaMiddleware';
-import { signUpSchema } from '../schemas/authSchema';
+import { signInSchema, signUpSchema } from '../schemas/authSchema';
 
 const authRouter = Router();
 
 authRouter.post('/signup', validateSchemaMiddleware(signUpSchema), signUp);
+authRouter.post('/signin', validateSchemaMiddleware(signInSchema), signIn);
 
 export default authRouter;
