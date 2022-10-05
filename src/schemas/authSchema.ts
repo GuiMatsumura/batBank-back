71e659011bd9604801fcd2af85293f5b094c005f
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2022-10-05 01:43:09 -0300

    :sparkles: feat: add schema for signup

---
+++ src/schemas/authSchema.ts
@@ -0,0 +1,9 @@
+import joi from 'joi';
+import { UserOmitBankNumber } from '../types/userType';
+
+export const signUpSchema = joi.object<UserOmitBankNumber>({
+  name: joi.string().required(),
+  email: joi.string().email().required(),
+  password: joi.string().min(3).required(),
+  pictureUrl: joi.string().required(),
+});
