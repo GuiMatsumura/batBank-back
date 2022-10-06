d625669d8e1f6c1b73dac1af838d60e51c17de35
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2022-10-06 02:17:22 -0300

    :sparkles: feat: add post transaction controller

--- src/controllers/balanceController.ts
+++ src/controllers/balanceController.ts
@@ -2,7 +2,9 @@ import { Request, Response } from 'express';
 import {
   getBalanceService,
   getTransactionsAndBalance,
+  createTransaction,
 } from '../services/balanceService';
+import dayjs from 'dayjs';
 
 export async function getBalance(req: Request, res: Response) {
   const { user } = res.locals;
@@ -17,4 +19,26 @@ export async function getTransactions(req: Request, res: Response) {
   const data = await getTransactionsAndBalance(user.id);
 
   res.status(200).send(data);
+}
+
+export async function postTransaction(req: Request, res: Response) {
+  const { user } = res.locals;
+  const {
+    amount,
+    description,
+    type,
+  }: { amount: number; description: string; type: string } = req.body;
+  const date = dayjs().format('HH:mm:ss DD/MM/YYYY');
+
+  const transaction = {
+    description,
+    type,
+    amount,
+    date,
+    userId: user.id,
+  };
+
+  await createTransaction(transaction);
+
+  res.sendStatus(201);
 }
