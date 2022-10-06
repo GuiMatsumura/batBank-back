4f933ce4462722756b60d724330c71281fc4ba22
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2022-10-06 02:18:13 -0300

    :sparkles: feat: add insert transaction repository

--- src/repositories/balanceRepository.ts
+++ src/repositories/balanceRepository.ts
@@ -1,9 +1,22 @@
 import { prisma } from '../database/prismaClient';
+import { ITransactions } from '../types/transactionTypes';
 
 export async function getTransactionsById(id: number) {
   return prisma.transactions.findMany({
     where: {
       userId: id,
+    },
+  });
+}
+
+export async function insertTransaction(transaction: ITransactions) {
+  return prisma.transactions.create({
+    data: {
+      description: transaction.description,
+      type: transaction.type,
+      amount: transaction.amount,
+      date: transaction.date,
+      userId: transaction.userId,
     },
   });
 }
