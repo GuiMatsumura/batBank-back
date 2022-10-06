79afc1f155b58bbb76e40e49ab6105dd9d2ca48f
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2022-10-06 00:28:02 -0300

    :sparkles: feat: add get transactions history controller

--- src/controllers/balanceController.ts
+++ src/controllers/balanceController.ts
@@ -1,9 +1,20 @@
 import { Request, Response } from 'express';
-import { getBalanceService } from '../services/balanceService';
+import {
+  getBalanceService,
+  getTransactionsAndBalance,
+} from '../services/balanceService';
 
 export async function getBalance(req: Request, res: Response) {
   const { user } = res.locals;
   const balance = await getBalanceService(user.id);
 
   res.status(200).send({ balance });
+}
+
+export async function getTransactions(req: Request, res: Response) {
+  const { user } = res.locals;
+
+  const data = await getTransactionsAndBalance(user.id);
+
+  res.status(200).send(data);
 }
