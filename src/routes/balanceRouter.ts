4b9f0dbadb6668a5c8a5d829df0bbf5620e1a63c
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2022-10-06 02:59:47 -0300

    :sparkles: feat: add transfer between users route

--- src/routes/balanceRouter.ts
+++ src/routes/balanceRouter.ts
@@ -3,8 +3,10 @@ import {
   getBalance,
   getTransactions,
   postTransaction,
+  postTransfer,
 } from '../controllers/balanceController';
 import { ensureAuthenticatedMiddleware } from '../middlewares/ensureAuthenticatedMiddleware';
+import { ensureTransferUser } from '../middlewares/ensureTransferUser';
 
 const balanceRouter = Router();
 
@@ -12,5 +14,6 @@ balanceRouter.use(ensureAuthenticatedMiddleware);
 balanceRouter.get('/balance', getBalance);
 balanceRouter.get('/transactions', getTransactions);
 balanceRouter.post('/transaction', postTransaction);
+balanceRouter.post('/transfer', ensureTransferUser, postTransfer);
 
 export default balanceRouter;
