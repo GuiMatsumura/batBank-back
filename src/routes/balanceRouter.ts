178b87e8e1668601a63b1ca5a39a863c0c7a992d
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2022-10-06 00:27:19 -0300

    :sparkles: feat: add get transactions to balance router

--- src/routes/balanceRouter.ts
+++ src/routes/balanceRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { getBalance } from '../controllers/balanceController';
+import { getBalance, getTransactions } from '../controllers/balanceController';
 import { ensureAuthenticatedMiddleware } from '../middlewares/ensureAuthenticatedMiddleware';
 
 const balanceRouter = Router();
 
 balanceRouter.use(ensureAuthenticatedMiddleware);
 balanceRouter.get('/balance', getBalance);
+balanceRouter.get('/transactions', getTransactions);
 
 export default balanceRouter;
