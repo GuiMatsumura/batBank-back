653d88c1c572746517a29c8223bf68d8357e6813
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2022-10-06 16:58:03 -0300

    :sparkles: feat: add transfer input schema

--- src/schemas/balanceSchema.ts
+++ src/schemas/balanceSchema.ts
@@ -1,7 +1,13 @@
 import joi from 'joi';
-import { ITransactionInput } from '../types/transactionTypes';
+import { ITransactionInput, ITransferInput } from '../types/transactionTypes';
 
 export const transactionSchema = joi.object<ITransactionInput>({
+  amount: joi.number().required(),
+  description: joi.string(),
+});
+
+export const transferSchema = joi.object<ITransferInput>({
+  email: joi.string().email().required(),
   amount: joi.number().required(),
   description: joi.string(),
 });
