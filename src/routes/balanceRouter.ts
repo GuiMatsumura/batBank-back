a429bb33aaa3551a9491fef155387aaa8a02a7d8
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2022-10-06 16:43:37 -0300

    :sparkles: feat: add validate schema middleware on post transaction

--- src/routes/balanceRouter.ts
+++ src/routes/balanceRouter.ts
@@ -7,13 +7,19 @@ import {
 } from '../controllers/balanceController';
 import { ensureAuthenticatedMiddleware } from '../middlewares/ensureAuthenticatedMiddleware';
 import { ensureTransferUser } from '../middlewares/ensureTransferUser';
+import { validateSchemaMiddleware } from '../middlewares/validateSchemaMiddleware';
+import { transactionSchema } from '../schemas/balanceSchema';
 
 const balanceRouter = Router();
 
 balanceRouter.use(ensureAuthenticatedMiddleware);
 balanceRouter.get('/balance', getBalance);
 balanceRouter.get('/transactions', getTransactions);
-balanceRouter.post('/transaction', postTransaction);
+balanceRouter.post(
+  '/transaction',
+  validateSchemaMiddleware(transactionSchema),
+  postTransaction
+);
 balanceRouter.post('/transfer', ensureTransferUser, postTransfer);
 
 export default balanceRouter;
