132f35681f25e06ed98e453d470e160c43a6e4b6
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2022-10-06 03:00:40 -0300

    :sparkles: feat: add transfer between users service

--- src/services/balanceService.ts
+++ src/services/balanceService.ts
@@ -3,24 +3,12 @@ import {
   insertTransaction,
 } from '../repositories/balanceRepository';
 import { ITransactions } from '../types/transactionTypes';
+import { calculateBalance } from '../utils/logicUtils';
 
 export async function getBalanceService(id: number) {
   const transactions = await getTransactionsById(id);
   const balance = calculateBalance(transactions);
 
-  return balance;
-}
-
-// type: any is a problem and i fix this soon
-async function calculateBalance(transactions: any) {
-  let balance = 0;
-  for (let i = 0; i < transactions.length; i++) {
-    if (transactions[i].type === '+') {
-      balance += transactions[i].amount;
-    } else {
-      balance -= transactions[i].amount;
-    }
-  }
   return balance;
 }
 
