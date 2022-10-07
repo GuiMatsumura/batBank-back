a88e7ae3f99c3046237c290b98a0c6e73acbf344
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2022-10-07 01:33:39 -0300

    :bug: fix: add type to transfer schema

--- src/schemas/balanceSchema.ts
+++ src/schemas/balanceSchema.ts
@@ -10,5 +10,6 @@ export const transactionSchema = joi.object<ITransactionInput>({
 export const transferSchema = joi.object<ITransferInput>({
   email: joi.string().email().required(),
   amount: joi.number().required(),
-  description: joi.string(),
+  description: joi.string().required(),
+  type: joi.string().required(),
 });
