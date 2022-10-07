69bb73a93daf11bb8e70c0cc5705d4257bcf703d
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2022-10-07 02:14:03 -0300

    :sparkles: feat: add get user name and user bank number controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -9,3 +9,9 @@ export async function putPictureUrl(req: Request, res: Response) {
 
   res.sendStatus(200);
 }
+
+export async function getUser(req: Request, res: Response) {
+  const { user } = res.locals;
+
+  res.status(200).send({ name: user.name, bankNumber: user.bankNumber });
+}
