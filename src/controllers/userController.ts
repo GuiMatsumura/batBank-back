6e329ad9463000c448287e98106a5c52a20818d0
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2022-10-07 02:03:25 -0300

    :sparkles: feat: add put picture url controller

---
+++ src/controllers/userController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+import { putPictureUrlService } from '../services/userService';
+
+export async function putPictureUrl(req: Request, res: Response) {
+  const { user } = res.locals;
+  const { pictureUrl }: { pictureUrl: string } = req.body;
+
+  await putPictureUrlService(user.id, pictureUrl);
+
+  res.sendStatus(200);
+}
