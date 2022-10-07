9f57c99c88f5837dd1e30225243f5258e2f62593
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2022-10-07 02:01:37 -0300

    :sparkles: feat: add user route to index router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import authRouter from './authRouter';
 import balanceRouter from './balanceRouter';
+import userRouter from './userRouter';
 
 const router = Router();
 
 router.use(authRouter);
 router.use(balanceRouter);
+router.use(userRouter);
 
 export default router;
