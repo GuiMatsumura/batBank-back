fbf5ff2c3d4fe5d4a1109060a7efaf07f5ab4d3a
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2022-10-12 05:03:49 -0300

    :sparkles: feat: add insert help service

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,5 +1,13 @@
-import { putPictureUrlRepository } from '../repositories/userRepository';
+import {
+  putPictureUrlRepository,
+  insertHelpRepository,
+} from '../repositories/userRepository';
+import { IHelpBody } from '../types/userType';
 
 export async function putPictureUrlService(id: number, pictureUrl: string) {
   await putPictureUrlRepository(id, pictureUrl);
+}
+
+export async function insertHelp(helpBody: IHelpBody) {
+  await insertHelpRepository(helpBody);
 }
