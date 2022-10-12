5dc15870b35323980b2eebe8e4db8bf71451e46c
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2022-10-12 05:04:12 -0300

    :sparkles: feat: add insert help repository

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -1,4 +1,5 @@
 import { prisma } from '../database/prismaClient';
+import { IHelpBody } from '../types/userType';
 
 export async function putPictureUrlRepository(id: number, pictureUrl: string) {
   return prisma.users.update({
@@ -7,6 +8,17 @@ export async function putPictureUrlRepository(id: number, pictureUrl: string) {
     },
     data: {
       pictureUrl,
+    },
+  });
+}
+
+export async function insertHelpRepository(helpBody: IHelpBody) {
+  return prisma.support.create({
+    data: {
+      message: helpBody.message,
+      date: helpBody.date,
+      userId: helpBody.userId,
+      email: helpBody.email,
     },
   });
 }
