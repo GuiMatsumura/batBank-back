0963f4bc185ed5a1bbb7f7d7310e37aa4577d655
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2022-10-12 05:04:49 -0300

    :sparkles: feat: create help schema

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -1,6 +1,11 @@
 import joi from 'joi';
-import { IPutPictureUrl } from '../types/userType';
+import { IPutPictureUrl, IHelpSchema } from '../types/userType';
 
 export const putPictureUrlSchema = joi.object<IPutPictureUrl>({
   pictureUrl: joi.string().required(),
+});
+
+export const insertHelpSchema = joi.object<IHelpSchema>({
+  message: joi.string().required(),
+  email: joi.string().email().required(),
 });
